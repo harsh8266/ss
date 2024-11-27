8f2dbc33ec75b5ec3ab337093e9f5f5084a0d232
Harsh8266 <58392509+harsh8266@users.noreply.github.com>
2024-11-27 19:30:22 +0530

    Update Diffie-Hellman.py

--- Diffie-Hellman.py
+++ Diffie-Hellman.py
@@ -1,3 +1,25 @@
+Algorithm:
+1. Choose Public Parameters:
+Both parties agree on a large prime number p and a base g
+(also called the generator), where g < p.
+2. Generate Private Keys:
+Each party selects a private key (a random integer) a and b,
+kept secret from each other.
+3. Compute Public Keys:
+Each party computes their public key:
+Party A computes A= g^a mod p
+Party B computes B= g^b mod p
+They then exchange these public keys over the insecure
+channel.
+4. Compute the Shared Secret:
+Each party then computes the shared secret using the other’s
+public key and their own private key:
+Party A computes s = B^a mod p
+Party B computes s = A^b mod p
+5. Both will arrive at the same shared secret s, which
+can be used for further encryption.
+
+
 # Diffie-Hellman Code
 
 # Power function to return value of a^b mod P
